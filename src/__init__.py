2b1fee22f4a734f3a0adaf4dec2df97c4842686b
v-danh <vdanhvt2000@gmail.com>
2023-06-17 21:19:37 +0700

    Modified __init__ file

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,11 @@
 if __name__ == '__main__':
     import sce
+    from clogs import logger
+    
+    sce.sum_of_squares(2, 2)
+    sce.subtract_of_squares(2, 2)
+    sce.subtract_two_squares(2, 2)
+    sce.sum_of_cubes(2, 2)
+    sce.subtract_of_cubes(2, 2)
+    sce.cube_two_sums(2, 2)
+    sce.cube_two_subtracts(2, 2)
