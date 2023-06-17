ffffee2103b29f68dc19ee375f82e6e5c3198a1e
v-danh <vdanhvt2000@gmail.com>
2023-06-17 21:48:44 +0700

    Modified something

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
     
 setup(
     name='sce',
-    version='0.0.1',
+    version='0.1.0',
     description='A Python library of seven memorable equality constants',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
